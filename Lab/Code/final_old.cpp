f809fc111294594e6cf94e51019e027df434332b
aqfort <valiev1379@gmail.com>
2020-05-18 22:48:59 +0500

    upgrade + method_2
    got rid of unnecessary memcpy of whole line (t = const) + another one method with different equation u(t,x)

--- Lab/Code/final_old.cpp
+++ Lab/Code/final_old.cpp
@@ -11,8 +11,8 @@
 
 #define A 0.5
 
-#define K 40
-#define M 20
+#define K 300
+#define M 100
 
 using namespace std;
 
@@ -27,18 +27,6 @@ double F_EXACT(const int &k,
 double U_EXPLICIT(const int &m,
                   double* DATA_LINE,
                   double* fooo_LINE);
-
-// ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
-
-double U_EXACT_2(const int &k,
-                 const int &m);
-
-double F_EXACT_2(const int &k,
-                 const int &m);
-
-double U_EXPLICIT_2(const int &m,
-                    double* DATA_LINE,
-                    double* fooo_LINE);
 
 void PRINT(double* DATA);
 
@@ -135,8 +123,6 @@ int main(int argc, char** argv) {
 
 // ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
 
-    MPI_Barrier(MPI_COMM_WORLD);
-
     double TIME_BEGIN = 0;
     double TIME_END = 0;
 
@@ -148,102 +134,52 @@ int main(int argc, char** argv) {
 
     free(SUBDATA);
 
+    SUBDATA = (double*) calloc((M - 1), sizeof(double));
+
+    double* DATA_LINE = (double*) calloc((M + 1), sizeof(double)); // Preceding line of DATA
+    double* fooo_LINE = (double*) calloc((M + 1), sizeof(double)); // Preceding line of fooo
+
     P = (M - 1) / SIZE;
 
-    SUBDATA = (double*) calloc(P, sizeof(double));
-
-    double* DATA_LINE = (double*) calloc((P + 2), sizeof(double)); // Preceding line of DATA
-    double* fooo_LINE = (double*) calloc((P + 2), sizeof(double)); // Preceding line of fooo
-
-    // Initialize DATA_LINE at k = 1
-
-    MPI_Scatter(&DATA[1], P, MPI_DOUBLE, &DATA_LINE[1], P, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-
-    MPI_Barrier(MPI_COMM_WORLD);
-
-    if(RANK == 0) {
-        for(int i = 0; i < SIZE; i++) {
-            MPI_Send(&DATA[P * i], 1, MPI_DOUBLE, i, 10, MPI_COMM_WORLD);
-            MPI_Send(&DATA[P * (i + 1) + 1], 1, MPI_DOUBLE, i, 20, MPI_COMM_WORLD);
-        }
-    }
-
-    {
-        MPI_Recv(&DATA_LINE[0], 1, MPI_DOUBLE, 0, 10, MPI_COMM_WORLD, NULL);
-        MPI_Recv(&DATA_LINE[P + 1], 1, MPI_DOUBLE, 0, 20, MPI_COMM_WORLD, NULL);
-    }
-
-    // 
-
     for(int k = 1; k <= K; k++) {
-        // Initialize fooo_LINE at each k
-
-        MPI_Scatter(&fooo[(M + 1) * (k - 1) + 1], P, MPI_DOUBLE, &fooo_LINE[1], P, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-
         MPI_Barrier(MPI_COMM_WORLD);
 
         if(RANK == 0) {
-            for(int i = 0; i < SIZE; i++) {
-                MPI_Send(&fooo[(M + 1) * (k - 1) + P * i], 1, MPI_DOUBLE, i, 10, MPI_COMM_WORLD);
-                MPI_Send(&fooo[(M + 1) * (k - 1) + P * (i + 1) + 1], 1, MPI_DOUBLE, i, 20, MPI_COMM_WORLD);
+            for(int m = 0; m <= M; m++ ) {
+                DATA_LINE[m] = DATA[(M + 1) * (k - 1) + m];
+                fooo_LINE[m] = fooo[(M + 1) * (k - 1) + m];
             }
         }
 
-        {
-            MPI_Recv(&fooo_LINE[0], 1, MPI_DOUBLE, 0, 10, MPI_COMM_WORLD, NULL);
-            MPI_Recv(&fooo_LINE[P + 1], 1, MPI_DOUBLE, 0, 20, MPI_COMM_WORLD, NULL);
-        }
-
-        //
-
-        MPI_Barrier(MPI_COMM_WORLD);
+        MPI_Bcast(DATA_LINE, (M + 1), MPI_DOUBLE, 0, MPI_COMM_WORLD);
+        MPI_Bcast(fooo_LINE, (M + 1), MPI_DOUBLE, 0, MPI_COMM_WORLD);
 
         for(int p = 0; p < P; p++) {
-            SUBDATA[p] = U_EXPLICIT(1 + p, DATA_LINE, fooo_LINE);
-        }
-
-        for(int p = 0; p < P; p++) { // Start init pre line DATA
-            DATA_LINE[p + 1] = SUBDATA[p];
+            SUBDATA[p] = U_EXPLICIT(1 + P * RANK + p, DATA_LINE, fooo_LINE);
         }
 
         if(RANK == 0) {
             for(int p = (M - 1) - (M - 1) % SIZE + 1; p < M; p++) {
-                DATA[(M + 1) * k + p] = U_EXPLICIT(p, &DATA[(M + 1) * (k - 1)], &fooo[(M + 1) * (k - 1)]);
+                DATA[(M + 1) * k + p] = U_EXPLICIT(p, DATA_LINE, fooo_LINE);
             }
         }
 
         MPI_Barrier(MPI_COMM_WORLD);
 
         MPI_Gather(SUBDATA, P, MPI_DOUBLE, &DATA[(M + 1) * k + 1], P, MPI_DOUBLE, 0, MPI_COMM_WORLD);
-
-        // Finish init pre line DATA
-
-        MPI_Barrier(MPI_COMM_WORLD);
-
-        if(RANK == 0) {
-            for(int i = 0; i < SIZE; i++) {
-                MPI_Send(&DATA[(M + 1) * k + P * i], 1, MPI_DOUBLE, i, 10, MPI_COMM_WORLD);
-                MPI_Send(&DATA[(M + 1) * k + P * (i + 1) + 1], 1, MPI_DOUBLE, i, 20, MPI_COMM_WORLD);
-            }
-        }
-
-        {
-            MPI_Recv(&DATA_LINE[0], 1, MPI_DOUBLE, 0, 10, MPI_COMM_WORLD, NULL);
-            MPI_Recv(&DATA_LINE[P + 1], 1, MPI_DOUBLE, 0, 20, MPI_COMM_WORLD, NULL);
-        }
     }
 
 // ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
 
     if(RANK == 0) {
-        cout << "░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░\n";
-        cout << "░░░░░░░░░░░░░░░░░░░░░░░░░DATA_EXACT░░░░░░░░░░░░░░░░░░░░░░░░░\n";
-        cout << "░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░\n";
-        PRINT(DATA_EXACT);
-        cout << "░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░\n";
-        cout << "░░░░░░░░░░░░░░░░░░░░░░░░░░░░DATA░░░░░░░░░░░░░░░░░░░░░░░░░░░░\n";
-        cout << "░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░\n";
-        PRINT(DATA);
+        // cout << "░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░\n";
+        // cout << "░░░░░░░░░░░░░░░░░░░░░░░░░DATA_EXACT░░░░░░░░░░░░░░░░░░░░░░░░░\n";
+        // cout << "░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░\n";
+        // PRINT(DATA_EXACT);
+        // cout << "░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░\n";
+        // cout << "░░░░░░░░░░░░░░░░░░░░░░░░░░░░DATA░░░░░░░░░░░░░░░░░░░░░░░░░░░░\n";
+        // cout << "░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░\n";
+        // PRINT(DATA);
     }
 
 // ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
@@ -258,7 +194,7 @@ int main(int argc, char** argv) {
         TIME_END = MPI_Wtime();
 
         cout << "░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░\n";
-        cout << "░░░░░░░░░░░░░░░░░░░░TIME SPENT: " << fixed << setprecision(7) << TIME_END - TIME_BEGIN << endl;
+        cout << "░░░░░░░░░░░░░░░░░░░░TIME SPENT: " << fixed << setprecision(7) << TIME_END - TIME_BEGIN << "...\n";
         cout << "░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░\n";
     }
 
@@ -284,15 +220,15 @@ double U_EXACT(const int &k,
 
 double F_EXACT(const int &k,
                const int &m) {
-    return (U_EXACT(k + 1, m) - U_EXACT(k, m)) * K / ((double) T) + 
-           (U_EXACT(k, m) - U_EXACT(k, m - 1)) * A * M * / ((double) X);
+    return (U_EXACT(k + 1, m) - 0.5 * (U_EXACT(k, m + 1) + U_EXACT(k, m - 1))) * K / ((double) T) + 
+           (U_EXACT(k, m + 1) - U_EXACT(k, m - 1)) * A * M * 0.5 / ((double) X);
 }
 
 double U_EXPLICIT(const int &m,
                   double* DATA_LINE,
                   double* fooo_LINE) {
-    return DATA_LINE[m] +
-           (fooo_LINE[m] - (DATA_LINE[m] - DATA_LINE[m - 1]) * A * M / ((double) X)) * T / ((double) K);
+    return 0.5 * (DATA_LINE[m + 1] + DATA_LINE[m - 1]) +
+           (fooo_LINE[m] - (DATA_LINE[m + 1] - DATA_LINE[m - 1]) * A * M * 0.5 / ((double) X)) * T / ((double) K);
 }
 
 void PRINT(double* DATA) {
@@ -308,34 +244,4 @@ void PRINT(double* DATA) {
 
     cout.precision(flag);
     cout << resetiosflags(ios::fixed);
-}
-
-// ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
-
-// ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
-
-// Another one method (most simple)
-
-// ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
-
-// ----- ----- ----- ----- ----- ----- ----- ----- ----- -----
-
-
-double U_EXACT_2(const int &k,
-                 const int &m) {
-    return -1 * pow((k * T / ((double) K) - 2), 2) +
-           pow((m * X / ((double) M - 1)), 2);
-}
-
-double F_EXACT_2(const int &k,
-                 const int &m) {
-    return (U_EXACT(k + 1, m) - U_EXACT(k, m)) * K / ((double) T) + 
-           (U_EXACT(k, m) - U_EXACT(k, m - 1)) * A * M / ((double) X);
-}
-
-double U_EXPLICIT_2(const int &m,
-                    double* DATA_LINE,
-                    double* fooo_LINE) {
-    return DATA_LINE[m] +
-           (fooo_LINE[m] - (DATA_LINE[m] - DATA_LINE[m - 1]) * A * M / ((double) X)) * T / ((double) K);
 }
